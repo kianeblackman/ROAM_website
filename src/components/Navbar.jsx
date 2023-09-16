ea6ba4d1d58191300655cdab1b12b009746ab888
kianeblackman <kianeblackman@gmail.com>
2023-09-16 16:42:34 +0100

    navbar now disapears on scroll and reappears on scroll up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,17 @@ import { Link } from 'react-router-dom';
 import roamLogo from '../assets/img/roam-logo-small.png';
 
 export default function NavBar() {
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar-wrapper").style.top = "0";
+        } else {
+            document.getElementById("navbar-wrapper").style.top = "-8rem";
+        }
+        prevScrollpos = currentScrollPos;
+    }
     return (
         <div id='navbar-wrapper'>
             <nav>
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -10,32 +10,43 @@ function Resources() {
     return (
         <div id='resources-wrapper'>
             <div id='resources-content'>
-                <div className="resource-section">
-                    <h2>LGBTQIA+</h2>
-                    <hr />
-                    <ul>
-                        {lgbtqResources.map((e, index) => (
-                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                        ))}
-                    </ul>
+                <div className="resources-intro">
+                    <h1>Resources</h1>
+                    <p className="large">We are dedicated to the promotion of diversity and inclusion in both the
+                        heritage world and the folk artistic movement. While this is not a complete
+                        list, it is a starting point. We will be managing an inclusion portal on our
+                        website in the near future. Please consider helping us by getting in contact
+                        to share other groups - of any size - that can be added to this list. For our
+                        most up-to-date list, as well as links to each source, click the link below.</p>
                 </div>
-                <div className="resource-section">
-                    <h2>Accessibility</h2>
-                    <hr />
-                    <ul>
-                        {accessibilityResources.map((e, index) => (
-                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                        ))}
-                    </ul>
-                </div>
-                <div className="resource-section">
-                    <h2>Communities Experiencing Racial Inequality</h2>
-                    <hr />
-                    <ul>
-                        {racialInequalityResources.map((e, index) => (
-                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                        ))}
-                    </ul>
+                <div className="resources-body">
+                    <div className="resource-section">
+                        <h2>LGBTQIA+</h2>
+                        <hr />
+                        <ul>
+                            {lgbtqResources.map((e, index) => (
+                                <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="resource-section">
+                        <h2>Accessibility</h2>
+                        <hr />
+                        <ul>
+                            {accessibilityResources.map((e, index) => (
+                                <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="resource-section">
+                        <h2>Communities Experiencing Racial Inequality</h2>
+                        <hr />
+                        <ul>
+                            {racialInequalityResources.map((e, index) => (
+                                <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
