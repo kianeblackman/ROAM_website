bb64be514493183c4e7fbaa885787bde30186f99
kianeblackman <kianeblackman@gmail.com>
2023-09-16 15:25:42 +0100

    ResourcesCard renders on Resources page and styling has been added

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -9,32 +9,34 @@ import ResourcesCard from "../components/ResourcesCard.jsx"
 function Resources() {
     return (
         <div id='resources-wrapper'>
-            <div>
-                <h1>LGBTQIA+</h1>
-                <hr />
-                <ul>
-                    {lgbtqResources.map((e, index) => (
-                        <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <h2>Accessibility</h2>
-                <hr />
-                <ul>
-                    {accessibilityResources.map((e, index) => (
-                        <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <h2>Communities Experiencing Racial Inequality</h2>
-                <hr />
-                <ul>
-                    {racialInequalityResources.map((e, index) => (
-                        <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
-                    ))}
-                </ul>
+            <div id='resources-content'>
+                <div className="resource-section">
+                    <h2>LGBTQIA+</h2>
+                    <hr />
+                    <ul>
+                        {lgbtqResources.map((e, index) => (
+                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                        ))}
+                    </ul>
+                </div>
+                <div className="resource-section">
+                    <h2>Accessibility</h2>
+                    <hr />
+                    <ul>
+                        {accessibilityResources.map((e, index) => (
+                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                        ))}
+                    </ul>
+                </div>
+                <div className="resource-section">
+                    <h2>Communities Experiencing Racial Inequality</h2>
+                    <hr />
+                    <ul>
+                        {racialInequalityResources.map((e, index) => (
+                            <ResourcesCard key={index} title={e.title} description={e.description} location={e.location} link={e.link} />
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
