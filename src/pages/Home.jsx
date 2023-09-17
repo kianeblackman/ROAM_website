6b35dcc27f3daec561e83edc00a7d195f3af46c3
kianeblackman <kianeblackman@gmail.com>
2023-09-17 13:33:50 +0100

    mobile resources section on home page amends

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,10 +39,10 @@ function Home() {
         <div id="resources-section">
 
           <div id="resources-details">
-            <div id="order-text">
+            <div id="resources-text">
               <h2>Resources</h2>
               <hr />
-              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
             <Button content="View Here" href="#" />
           </div>
